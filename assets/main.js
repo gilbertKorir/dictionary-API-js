52d109a26e8a8358e1180e97fcd4680c64436189
gilbertKorir <jerikorir@gmail.com>
2022-06-28 20:19:06 +0300

    ft:added the js functionalities

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,35 @@
-const url ="";
+const base_url ="https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () =>{
     let inpword = document.getElementById("inp-word").value;
-    console.log(inpword);
+    // console.log(inpword);
 
-    fetch(`${url}${inpword}`)
+    fetch(`${base_url}${inpword}`)
           .then((response) => response.json)
-          .then((data) => console.log(data))
+          .then((data) => { 
+            console.log(data)
+    
+            result.innerHTML = `
+            <div class="word">
+            <h3>${inpword}</h3>
+
+          <button>
+            <i class="fa-solid fa-volume"></i>
+          </button>
+        </div>
+        <div class="details">
+            <p>pos</p>
+            <p>/sample/</p>
+        </div>
+        <p class="word-meaning">
+            Lorem ipsum dolor sit
+        </p>
+        <p class="word-example">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, quos.
+        </p>`
+        })
 
 });
